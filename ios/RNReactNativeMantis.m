ae2b27d5071454036234869d62ea9b9ecf090f22
Patryk Szczyglo <patryk.szczyglo@netguru.pl>
2019-10-10 19:10:18 +0200

    Removed bad implementation of @end

--- ios/RNReactNativeMantis.m
+++ ios/RNReactNativeMantis.m
@@ -1,15 +1,5 @@
 
 #import "RNReactNativeMantis.h"
 
-@implementation RNReactNativeMantis
-
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
-RCT_EXPORT_MODULE()
-
 @interface RCT_EXTERN_MODULE(RNMantis, NSObject)
-@end
-
 @end
